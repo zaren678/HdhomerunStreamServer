21c507aea2b55301f905c6582306ca8553a28ce8
John Anderson <zaren678@gmail.com>
2012-10-22 20:56:37 -0700

    remove extra stuff, add executables

--- Windows/HdhomerunStreamServer/HdhomerunStreamServer/Properties/AssemblyInfo.cs
+++ Windows/HdhomerunStreamServer/HdhomerunStreamServer/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApplication1")]
+[assembly: AssemblyTitle("Hdhomerun Signal Meter Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
